217a9628cc86d4725e70fdeb4b9645aaaf5125a3
sebastiaolsousa <sebastiao.luzsousa@gmail.com>
2023-05-25 21:01:55 -0300

    Update

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+var divlist = document.getElementById("list-creation");
+
 function createlist() {
     var listname = document.getElementById("listname").value;
   
@@ -5,33 +7,90 @@ function createlist() {
     var textlistname = document.createTextNode(listname);
     h1listname.appendChild(textlistname);
   
-    var divlist = document.getElementById("list-creation");
     var mainlist = document.querySelector("main");
     divlist.innerHTML = "";
     divlist.appendChild(h1listname);
     divlist.classList.add("expanded")
     mainlist.classList.add("expanded")
-    
-    
 
-    //var todoparagraph = document.createElement("input:checkbox")
+    addactivity();
+
   }
 
 function addactivity(){
   var inputactivity = document.createElement("input")
   inputactivity.type = "text"
+  inputactivity.classList.add("expanded")
+  inputactivity.id = "activity"
 
   var buttonactivity = document.createElement("button")
-  buttonactivity.textContent = "Create"
+  buttonactivity.textContent = "To-Do"
   buttonactivity.addEventListener("click", function() {
-    addactivity();
+    insertactivity();
   })
+  buttonactivity.classList.add("expanded")
+
+  divlist.appendChild(inputactivity)
+  divlist.appendChild(buttonactivity) 
 }
 
-function clean(){
-    
+function insertactivity() {
+  var inputactivity = document.getElementById("activity");
+  var activityText = inputactivity.value;
+
+  var paragraph = document.createElement("p");
+  var checkbox = document.createElement("input");
+  checkbox.type = "checkbox";
+
+  var textNode = document.createTextNode(activityText);
+  paragraph.appendChild(checkbox);
+  paragraph.appendChild(textNode);
+
+
+  checkbox.addEventListener("change", function() {
+    if (this.checked) {
+      paragraph.style.textDecoration = "line-through";
+      paragraph.style.color = "gray";
+    } else {
+      paragraph.style.textDecoration = "none";
+      paragraph.style.color = "black";
+    }
+  });
+
+  var divActivities = document.createElement("div");
+  divActivities.appendChild(paragraph);
+  divlist.appendChild(divActivities);
 }
 
+function clean() {
+  // Limpar o conteúdo da divlist
+  divlist.innerHTML = "";
+
+  // Remove as classes "expanded" das divs
+  divlist.classList.remove("expanded");
+  var mainlist = document.querySelector("main");
+  mainlist.classList.remove("expanded");
+
+  // Criar um novo input type text
+  var input = document.createElement("input");
+  input.type = "text";
+  input.name = "listname";
+  input.id = "listname";
+  input.placeholder = "Name your list...";
+  input.maxLength = 20;
+
+  // Criar um novo botão
+  var button = document.createElement("button");
+  button.textContent = "Create List";
+  button.addEventListener("click", function() {
+    createlist();
+  });
+  button.style.marginLeft = "4px"
+
+  // Adicionar o novo input e botão à divlist
+  divlist.appendChild(input);
+  divlist.appendChild(button);
+}
 function print(){
     
 }
