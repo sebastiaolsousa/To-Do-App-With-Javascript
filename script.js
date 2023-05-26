145868d25bfc137ec115c334ecb760b27055de43
sebastiaolsousa <sebastiao.luzsousa@gmail.com>
2023-05-26 11:14:55 -0300

    Ending?

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
 var divlist = document.getElementById("list-creation");
 
 function createlist() {
-    var listname = document.getElementById("listname").value;
-  
+  var listname = document.getElementById("listname").value;
+  if (listname === '') {
+    window.alert('Your List Needs a Name')
+  } else {
     var h1listname = document.createElement("h1");
     var textlistname = document.createTextNode(listname);
     h1listname.appendChild(textlistname);
@@ -10,28 +12,35 @@ function createlist() {
     var mainlist = document.querySelector("main");
     divlist.innerHTML = "";
     divlist.appendChild(h1listname);
-    divlist.classList.add("expanded")
-    mainlist.classList.add("expanded")
-
+    divlist.classList.add("expanded");
+    mainlist.classList.add("expanded");
+  
     addactivity();
-
   }
+}
 
 function addactivity(){
-  var inputactivity = document.createElement("input")
-  inputactivity.type = "text"
-  inputactivity.classList.add("expanded")
-  inputactivity.id = "activity"
+  var inputactivity = document.createElement("input");
+  inputactivity.type = "text";
+  inputactivity.classList.add("expanded");
+  inputactivity.id = "activity";
+  inputactivity.maxLength = "33";
+  inputactivity.placeholder = "Write your activity...";
 
-  var buttonactivity = document.createElement("button")
-  buttonactivity.textContent = "To-Do"
+  var buttonactivity = document.createElement("button");
+  buttonactivity.textContent = "To-Do";
   buttonactivity.addEventListener("click", function() {
-    insertactivity();
-  })
-  buttonactivity.classList.add("expanded")
+    if (inputactivity.value === '') {
+      window.alert('Your Activity Needs a Name');
+    } else {
+      insertactivity();
+      inputactivity.value = '';
+    }
+  });
+  buttonactivity.classList.add("expanded");
 
-  divlist.appendChild(inputactivity)
-  divlist.appendChild(buttonactivity) 
+  divlist.appendChild(inputactivity);
+  divlist.appendChild(buttonactivity);
 }
 
 function insertactivity() {
@@ -63,15 +72,12 @@ function insertactivity() {
 }
 
 function clean() {
-  // Limpar o conteúdo da divlist
   divlist.innerHTML = "";
 
-  // Remove as classes "expanded" das divs
   divlist.classList.remove("expanded");
   var mainlist = document.querySelector("main");
   mainlist.classList.remove("expanded");
 
-  // Criar um novo input type text
   var input = document.createElement("input");
   input.type = "text";
   input.name = "listname";
@@ -79,7 +85,6 @@ function clean() {
   input.placeholder = "Name your list...";
   input.maxLength = 20;
 
-  // Criar um novo botão
   var button = document.createElement("button");
   button.textContent = "Create List";
   button.addEventListener("click", function() {
@@ -87,10 +92,22 @@ function clean() {
   });
   button.style.marginLeft = "4px"
 
-  // Adicionar o novo input e botão à divlist
   divlist.appendChild(input);
   divlist.appendChild(button);
 }
 function print(){
-    
+  var clonedDiv = divlist.cloneNode(true);
+
+  var inputElements = clonedDiv.querySelectorAll("input[type='text'], button");
+  inputElements.forEach(function(element) {
+    element.parentNode.removeChild(element);
+  });
+
+  var divcontent = clonedDiv.innerHTML;
+  var printWindow = window.open('', '', 'height=500,width=500');
+  printWindow.document.write('<html><head><title>Print</title></head><body>');
+  printWindow.document.write(divcontent);
+  printWindow.document.write('</body></html>');
+  printWindow.document.close();
+  printWindow.print();
 }
